bab586d24a6291a3fb5a0e901c4884c9a4f145bb
backspacf <55361937+backspacf@users.noreply.github.com>
2019-12-28 11:41:56 -0800

    Update bigHTM.js
    hm

--- bigHTM.js
+++ bigHTM.js
@@ -4,8 +4,5 @@ this is a javascript library built to increase ability of html by adding feature
 
     ALPHA-0.01. all this is is a thing that writes all hyper-text markup out of the script tag. more to come.
  */
-function bigHTM(){
-  var mHTM = document.getElementById('olHTM').innerHTML;
-  document.write(mHTM);
-};
-bigHTM();
+var mHTM = document.getElementById('olHTM').innerHTML;
+document.write(mHTM);
