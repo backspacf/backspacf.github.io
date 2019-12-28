117a5b8cca625b63957bc1ed4607a7d823fc4af2
backspacf <55361937+backspacf@users.noreply.github.com>
2019-12-28 11:32:56 -0800

    Update bigHTM.js
    innerHTML

--- bigHTM.js
+++ bigHTM.js
@@ -14,9 +14,8 @@ have fun, and don't die. visit me at https://backspacf.github.io
     ALPHA-0.01. all this is is a thing that writes all hyper-text markup out of the script tag. more to come.
  */
 function bigHTM(){
-  var mHTM = document.getElementById('bigHTM').value;
+  var mHTM = document.getElementById('bigHTM').innerHTML;
   document.write(mHTM);
   alert(mHTM);
-  document.write("bruh"); 
 };
 bigHTM();
