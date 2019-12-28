4f85b3b6c0b037c0dc13a2eb3a97309348e02880
backspacf <55361937+backspacf@users.noreply.github.com>
2019-12-28 10:20:46 -0800

    bigHTM.js
    first attempt at a html library

--- bigHTM.js
+++ bigHTM.js
@@ -6,7 +6,7 @@ to use:
  - if your file is .html (which is recommended), make sure to add a <!DOCTYPE html>.
    then add a <script> tag from beginning to end (excluding DOCTYPE). write your html within the script. make sure your script tag has the id "bigHTM".
    write your js within the script as well.
-   also outside of the main script tag, add another script tag to include bigHTM (recommended to use my link: //backspacf.github.io/js/bigHTM.min.js):
+   also outside of the main script tag, add another script tag to include bigHTM (recommended to use my link: //backspacf.github.io/js/bigHTM.js):
    <script src="(bigHTM source)"></script>
  - if your file is .js (which is not recommended unless you know what you are doing), simply write your brand-new htm code.
    full documentation here: https://backspacf.github.io/bigHTM/docs.html
