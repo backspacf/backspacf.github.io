558256f26be3a823016122a3cbb7068d7d247ebf
backspacf <55361937+backspacf@users.noreply.github.com>
2019-12-28 11:36:11 -0800

    Update bigHTM.js
    olHTM

--- bigHTM.js
+++ bigHTM.js
@@ -1,20 +1,11 @@
 /*
 bigHTM.js by joj-the-egg.
 this is a javascript library built to increase ability of html by adding features.
-to use:
-
- - if your file is .html (which is recommended), make sure to add a <!DOCTYPE html>.
-   then add a <html> tag from beginning to end (excluding DOCTYPE). write everything within this html tag.
-   outside of the main html tag, add a script tag to include bigHTM (recommended to use my link: //backspacf.github.io/js/bigHTM.js):
-   <script src="(bigHTM source)"></script>
-
-
-have fun, and don't die. visit me at https://backspacf.github.io
 
     ALPHA-0.01. all this is is a thing that writes all hyper-text markup out of the script tag. more to come.
  */
 function bigHTM(){
-  var mHTM = document.getElementById('bigHTM').innerHTML;
+  var mHTM = document.getElementById('olHTM').innerHTML;
   document.write(mHTM);
   alert(mHTM);
 };
