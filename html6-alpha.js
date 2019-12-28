07e7b8d389e8a6c651acc7f81ee53e3ae7a96e44
backspacf <55361937+backspacf@users.noreply.github.com>
2019-12-28 10:19:36 -0800

    bigHTM.js
    first attempt at html library

---
+++ html6-alpha.js
@@ -0,0 +1,21 @@
+/*
+bigHTM.js by joj-the-egg.
+this is a javascript library built to increase ability of html by adding features.
+to use:
+
+ - if your file is .html (which is recommended), make sure to add a <!DOCTYPE html>.
+   then add a <script> tag from beginning to end (excluding DOCTYPE). write your html within the script. make sure your script tag has the id "bigHTM".
+   write your js within the script as well.
+   also outside of the main script tag, add another script tag to include bigHTM (recommended to use my link: //backspacf.github.io/js/bigHTM.min.js):
+   <script src="(bigHTM source)"></script>
+ - if your file is .js (which is not recommended unless you know what you are doing), simply write your brand-new htm code.
+   full documentation here: https://backspacf.github.io/bigHTM/docs.html
+
+have fun, and don't die. visit me at https://backspacf.github.io
+
+    ALPHA-0.01. all this is is a thing that writes all hyper-text markup out of the script tag. more to come.
+ */
+(function(){
+  document.write(document.getElementById(bigHTM));
+  //writes all htm in this script tag.
+});
